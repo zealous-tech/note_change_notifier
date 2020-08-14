65993da36a4853545f4e4a8342d9e1d8ebdc54ef
Suren Grigoryan <suren@instigatemobile.com>
2020-08-14 15:58:27 +0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# note_change_notifier
-Note Change Notifier is a Redmine plugin that notifies note change of contact/deal in redmine_contacts' plugin by e-mail.
+# Journal Change Notifier
+
+Journal Change Notifier is a Redmine plugin that notifies journal
+change by e-mail.
+
+## License
+
+GPL 2 or later. See LICENSE for details.
+
+# Note Change Notifier
+
+Note Change Notifier is a Redmine plugin that notifies note
+change of contact/deal in redmine_contacts' plugin by e-mail. Note Change Notifier
+is based on (modified) Journal Change Notifier. Plugin tested on redmine 
+version 3.4.2.
+
+# Installation of note_change_notifier plugin
+
+1. Install plugin as usual:
+<pre>
+$ sudo cp -r note_change_notifier [Redmine_Root]/plugins
+$ cd [Redmine_Root]
+$ sudo bundle install
+$ sudo bundle exec rake redmine:plugins:migrate RAILS_ENV=production
+$ sudo /etc/init.d/apache2 restart
+</pre>
+2. Edit [Redmine_Root]/plugins/redmine_contacts/app/controllers/notes_controller.rb (add call_hook to update and destroy methods), as follows:
+<pre>
+...
+...
+...
+  def update
+    @note.safe_attributes = params[:note]
+    if @note.save
+      @note.note_time = params[:note][:note_time] if params[:note] && params[:note][:note_time]
+      attachments = Attachment.attach_files(@note, (params[:attachments] || (params[:note] && params[:note][:uploads])))  
+      render_attachment_warning_if_needed(@note)
+      flash[:notice] = l(:notice_successful_update)
+      respond_to do |format|
+        format.html { redirect_back_or_default({ :action => 'show', :project_id => @note.source.project, :id => @note }) }
+        format.api  { render_api_ok }
+      end 
++     call_hook(:plugin_redmine_contacts_controller_notes_edit_post, { :note => @note, :params => params}) if Redmine::Plugin.installed?(:note_change_notifier)
+    else
+      respond_to do |format|
+        format.html { render :action => 'edit', :project_id => params[:project_id], :id => @note }
+        format.api  { render_validation_errors(@note) }
+      end
+    end
+  end
+...
+...
+...
+  def destroy
+    (render_403; return false) unless @note.destroyable_by?(User.current, @project)
+    @note.destroy
+    respond_to do |format|
+      format.js
+      format.html { redirect_to :action => 'show', :project_id => @project, :id => @note.source }
+      format.api  { render_api_ok }
+    end
++   call_hook(:plugin_redmine_contacts_controller_notes_edit_post, { :note => @note, :params => params}) if Redmine::Plugin.installed?(:note_change_notifier)
+    # redirect_to :action => 'show', :project_id => @project, :id => @contact
+  end
+...
+...
+...
+</pre>
+
+# Plugin's issue in internal redmine
+
+https://intranet.instigatemobile.com/issues/12990
---
+++ init.rb
@@ -0,0 +1,105 @@
+Redmine::Plugin.register :note_change_notifier do
+  name "Note Change Notifier plugin"
+  author "Kouhei Sutou"
+  description "Notify note change by e-mail"
+  version "1.0.0"
+  url "https://github.com/clear-code/redmine-plugin-journal-change-notifier"
+  author_url "https://github.com/kou/"
+end
+
+require "diff/lcs"
+require "diff/lcs/hunk"
+
+class NoteChangeMailer < Mailer
+  class << self
+    def deliver_note_edit(changer, note)
+      @note = note
+      recipients = note.source.recipients
+      to = recipients
+      cc = (note.source.respond_to?(:all_watcher_recepients) ? note.source.all_watcher_recepients : note.source.watcher_recipients) - recipients
+      mail = note_edit(changer,
+                       note,
+                       to & recipients,
+                       cc & cc)
+      mail.deliver
+    end
+  end
+
+  def note_edit(changer, note, to_users, cc_users)
+    redmine_headers 'Project' => note.source.project.identifier,
+                    'X-Notable-Id' => note.source.id,
+                    'X-Note-Id' => note.id
+    @author = note.author
+    @changer = changer
+    message_id note
+    recipients = note.source.recipients
+    cc = (note.source.respond_to?(:all_watcher_recepients) ? note.source.all_watcher_recepients : note.source.watcher_recipients) - recipients
+    @note = note
+    #@note_details = note.visible_details(user)
+    @note_url = url_for(:controller => 'notes', :action => 'show', :id => note.id)
+    mail :to => recipients,
+         :cc => cc,
+         #:subject => "[#{note.source.project.name}] - #{l(:label_crm_note_for)} #{note.source.name}"
+         :subject => "[#{note.source.project.name} - #{note.source_type}  ##{note.source.id}] #{l(:label_crm_note_for)} #{note.source.name}"
+  end
+end
+
+class NoteChangeDiffer
+  def initialize(note)
+    @note = note
+  end
+
+  def diff
+    if @note.previous_changes.key?("content")
+      from, to, = @note.previous_changes["content"]
+    else
+      from = @note.content
+      to = ""
+    end
+    unified_diff(from, to)
+  end
+
+  private
+  def unified_diff(content_from, content_to)
+    to_lines = content_to.lines.collect(&:chomp)
+    from_lines = content_from.lines.collect(&:chomp)
+    diffs = ::Diff::LCS.diff(from_lines, to_lines)
+
+    unified_diff = ""
+
+    old_hunk = nil
+    n_lines = 3
+    format = :unified
+    file_length_difference = 0
+    diffs.each do |piece|
+      begin
+        hunk = ::Diff::LCS::Hunk.new(from_lines, to_lines, piece, n_lines,
+                                     file_length_difference)
+        file_length_difference = hunk.file_length_difference
+
+        next unless old_hunk
+
+        if (n_lines > 0) and hunk.overlaps?(old_hunk)
+          hunk.merge(old_hunk)
+        else
+          unified_diff << old_hunk.diff(format)
+        end
+      ensure
+        old_hunk = hunk
+        unified_diff << "\n"
+      end
+    end
+
+    unified_diff << old_hunk.diff(format)
+    unified_diff << "\n"
+    unified_diff
+  end
+end
+
+class NoteDiffNotifyListener < Redmine::Hook::Listener
+  def plugin_redmine_contacts_controller_notes_edit_post(context)
+    note = context[:note]
+    #return unless note.previous_changes.key?("content")
+    NoteChangeMailer.deliver_note_edit(User.current, note)
+  end
+end
